c3c6506a25b049807aab5cc1a11b64fa95f3a4c6
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 09:52:26 +0200

    Initial setup

--- includes/bashrc.sh
+++ includes/bashrc.sh
@@ -1,11 +1,11 @@
 # Define custom prompt
-export PS1='\[\033[01;35m\]\u\[\033[00m\] @ \[\033[01;32m\]\h\[\033[00m\] in \[\033[01;34m\]\w\[\033[00m\] '
+export PS1='\[\033[01;35m\]\u\[\033[00m\] @ \[\033[01;32m\]\h\[\033[00m\] in \[\033[01;34m\]\w\[\033[00m\]: '
 
 # Define the gitprompt
 # https://github.com/magicmonty/bash-git-prompt
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
-    GIT_PROMPT_START="${PS1}on"
-    GIT_PROMPT_END=""
+    GIT_PROMPT_START="${PS1:0:-2} on"
+    GIT_PROMPT_END=":"
     source $HOME/.bash-git-prompt/gitprompt.sh
 fi
