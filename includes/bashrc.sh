57b01229337196659e4b2c6fccf02fb64a0c5e02
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 09:52:59 +0200

    Initial setup

--- includes/bashrc.sh
+++ includes/bashrc.sh
@@ -6,6 +6,6 @@ export PS1='\[\033[01;35m\]\u\[\033[00m\] @ \[\033[01;32m\]\h\[\033[00m\] in \[\
 if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     GIT_PROMPT_START="${PS1:0:-2} on"
-    GIT_PROMPT_END=":"
+    GIT_PROMPT_END=": "
     source $HOME/.bash-git-prompt/gitprompt.sh
 fi
