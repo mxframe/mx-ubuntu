81dd7bfc8c8cf9bf70ead5f0c5385b0e261d291c
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 10:55:00 +0200

    Initial setup

--- includes/aliases.sh
+++ includes/aliases.sh
@@ -4,11 +4,12 @@
 sysInfo () {
     printf "CPU: "
     cat /proc/cpuinfo | grep "model name" | head -1 | awk '{ for (i = 4; i <= NF; i++) printf "%s ", $i }'
+    echo ""
     printf "\n"
-    cat /etc/issue | awk '{ printf "OS: %s %s %s %s | " , $1 , $2 , $3 , $4 }'
+    cat /etc/issue | awk '{ printf "OS: %s %s %s %s" , $1 , $2 , $3 , $4 }'
     printf "\n"
     uname -a | awk '{ printf "Kernel: %s " , $3 }'
-    uname -m | awk '{ printf "%s | " , $1 }'
+    uname -m | awk '{ printf "%s" , $1 }'
     printf "\n"
     kded4 --version | grep "KDE Development Platform" | awk '{ printf "KDE: %s", $4 }'
     printf "\n"
