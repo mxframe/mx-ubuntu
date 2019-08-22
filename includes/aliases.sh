75434a10eb36be0d8d14663105acce147673b410
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 10:48:24 +0200

    Initial setup

--- includes/aliases.sh
+++ includes/aliases.sh
@@ -4,17 +4,17 @@
 sysInfo () {
     printf "CPU: "
     cat /proc/cpuinfo | grep "model name" | head -1 | awk '{ for (i = 4; i <= NF; i++) printf "%s ", $i }'
-    echo
+    echo -e "\n"
 
     cat /etc/issue | awk '{ printf "OS: %s %s %s %s | " , $1 , $2 , $3 , $4 }'
     uname -a | awk '{ printf "Kernel: %s " , $3 }'
     uname -m | awk '{ printf "%s | " , $1 }'
     kded4 --version | grep "KDE Development Platform" | awk '{ printf "KDE: %s", $4 }'
-    echo
+    echo -e "\n"
     uptime | awk '{ printf "Uptime: %s %s %s", $3, $4, $5 }' | sed 's/,//g'
-    echo
-    cputemp | head -1 | awk '{ printf "%s %s %s\n", $1, $2, $3 }'
-    cputemp | tail -1 | awk '{ printf "%s %s %s\n", $1, $2, $3 }'
+    echo -e "\n"
+    cputemp | head -1 | awk '{ echo -e "%s %s %s\n", $1, $2, $3 }'
+    cputemp | tail -1 | awk '{ echo -e "%s %s %s\n", $1, $2, $3 }'
 }
 
 # ================================================
@@ -25,7 +25,7 @@ alias cpuInfo='cat /proc/cpuinfo'
 # ================================================
 # Function to make a git pull and source the .bashrc
 # ================================================
-alias pullAndSource='git pull & source ~/.bashrc'
+alias pullAndSource='git pull && source ~/.bashrc'
 
 # ================================================
 # Apache aliases
