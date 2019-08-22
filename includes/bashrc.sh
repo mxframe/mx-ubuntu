22d504c4dedafe58daf8775a87bfeba9a94a022a
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 11:41:30 +0200

    Initial setup

--- includes/bashrc.sh
+++ includes/bashrc.sh
@@ -1,3 +1,3 @@
 # Load the bashrc includes
-source $HOME/bin/server-setup/includes/styles.sh
-source $HOME/bin/server-setup/includes/aliases.sh
+source $HOME/bin/server-setup/includes/bashrc/styles.sh
+source $HOME/bin/server-setup/includes/bashrc/aliases.sh
