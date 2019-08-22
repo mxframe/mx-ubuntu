f94c5c53518092e3c8d223f13e5c220ea5cccd59
Mathias Berg <m.berg@spectrum8.de>
2019-08-22 11:00:18 +0200

    Initial setup

--- includes/aliases.sh
+++ includes/aliases.sh
@@ -5,7 +5,7 @@ sysInfo () {
     printf "CPU: "
     cat /proc/cpuinfo | grep "model name" | head -1 | awk '{ for (i = 4; i <= NF; i++) printf "%s ", $i }'
     printf "\n"
-    cat /etc/issue | awk '{ printf "OS: %s %s %s %s - " , $1 , $2 , $3 }'
+    cat /etc/issue | awk '{ printf "OS: %s %s %s - " , $1 , $2 , $3 }'
     printf "\n"
     uname -a | awk '{ printf "Kernel: %s " , $3 }'
     uname -m | awk '{ printf "%s" , $1 }'
