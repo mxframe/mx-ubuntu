3546193781461720db4ffa8e4f4c693a0a9d9ecb
Mathias Berg <m.berg@spectrum8.de>
2019-09-05 18:41:12 +0200

    Initial setup

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if /etc/apache2/sites-available is symlinked
-if [[ -L /etc/apache2/sites-available ]]
+if [[ ! -L /etc/apache2/sites-available ]]
 then
     sudo rm -rf /etc/apache2/sites-available
     sudo ln -s /webcluster-share/apache2/sites-available /etc/apache2/sites-available
 fi
 
 # Check if /etc/apache2/sites-enabled is symlinked
-if [[ -L /etc/apache2/sites-enabled ]]
+if [[ ! -L /etc/apache2/sites-enabled ]]
 then
     sudo rm -rf /etc/apache2/sites-enabled
     sudo ln -s /webcluster-share/apache2/sites-enabled /etc/apache2/sites-enabled
