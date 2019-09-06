d6b31240eeabc874e6859a8bd5b0ff5d7d5c6edb
Mathias Berg <m.berg@spectrum8.de>
2019-09-06 13:15:04 +0200

    Initial setup

--- source-bash.sh
+++ source-bash.sh
@@ -4,15 +4,17 @@
 pathBash="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Source the bash file
-if [[ ! -L /etc/profile.d/mx-ubuntu.bashrc ]]
+if [[ -L /etc/profile.d/mx-ubuntu.bashrc ]]
 then
     rm -rf /etc/profile.d/mx-ubuntu.bashrc 2>/dev/null
-    if [[ ${pathBash} = '/bash-projects/mxframe/mx-ubuntu' ]]
-    then
-        ln -s "/bash-projects/mxframe/mx-ubuntu/bashrc.sh" /etc/profile.d/mx-ubuntu.bashrc 2>/dev/null
-    else
-        ln -s "/usr/local/packages/mx-ubuntu/bashrc.sh" /etc/profile.d/mx-ubuntu.bashrc 2>/dev/null
-    fi
+fi
+
+# Create the symlink
+if [[ ${pathBash} = '/bash-projects/mxframe/mx-ubuntu' ]]
+then
+    ln -s "/bash-projects/mxframe/mx-ubuntu/bashrc.sh" /etc/profile.d/mx-ubuntu.bashrc 2>/dev/null
+else
+    ln -s "/usr/local/packages/mx-ubuntu/bashrc.sh" /etc/profile.d/mx-ubuntu.bashrc 2>/dev/null
 fi
 
 # Source the users bashrc
