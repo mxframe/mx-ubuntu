422b8b365bcf29032ce4b8e77ac8a079c81e8a4d
Mathias Berg <m.berg@spectrum8.de>
2019-09-06 10:31:43 +0200

    Initial setup

--- self-update.sh
+++ self-update.sh
@@ -17,7 +17,7 @@ then
 fi
 
 # Change the permissions
-sudo chmod -R 440 * 2>/dev/null
+sudo chmod -R 660 * 2>/dev/null
 sudo chmod 770 self-update.sh 2>/dev/null
 sudo chmod 770 bashrc.sh 2>/dev/null
 sudo chmod 770 start.sh 2>/dev/null
