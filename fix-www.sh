19bb300857a46ae8dd3fc7dc95173a4021c1a143
Mathias Berg <m.berg@spectrum8.de>
2019-10-01 07:13:14 +0200

    Initial setup

--- fix-www.sh
+++ fix-www.sh
@@ -5,10 +5,10 @@ if [[ -d /var/www/html ]]
 then
     # Change owner
     sudo chown www-data:www-data /var/www/html
-    sudo chgrp -R www-data /var/www/html
+#    sudo chgrp -R www-data /var/www/html
     sudo chmod -R 775 /var/www/html
     # https://askubuntu.com/questions/51951/set-default-group-for-user-when-they-create-new-files
     sudo chgrp -R www-data /var/www/html
     sudo chmod -R g+s /var/www/html
-    sudo chown -R $(whoami) /var/www/html
+#    sudo chown -R $(whoami) /var/www/html
 fi
